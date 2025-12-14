9d9536d17ca9e5897ce14f867d18bb0688583249
kupidones <kupidones@mail.ru>
2025-12-15 00:56:38 +1000

    Add StudentManager class to process student data

---
+++ 3_studentmanag.cs
@@ -0,0 +1,8 @@
+internal class StudentManager
+{
+    internal void ProcessStudentData(Student student)
+    {
+        Console.WriteLine("Processing student data...");
+        student.PrintStudentInfo();
+    }
+}
